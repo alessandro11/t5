cb90a76cf8da50729d2d9f06c96a8021962d0f57
Alessandro Elias <ae11@c3sl.ufpr.br>
2017-06-14 19:08:12 -0300

    Add shoe CRUD.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

---
+++ app/models/shoe.rb
@@ -0,0 +1,3 @@
+class Shoe < ActiveRecord::Base
+  belongs_to :person
+end
---
+++ db/migrate/20170614172833_create_shoes.rb
@@ -0,0 +1,11 @@
+class CreateShoes < ActiveRecord::Migration
+  def change
+    create_table :shoes do |t|
+      t.string :manufacturer
+      t.integer :size
+      t.references :person, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
